40918e43ea3beb7e480c6b3a5ef2f7ba32c83544
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2026-03-03 14:47:03 +0100

    using George's input

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-python train.py \
+## CUDA_VISIBLE_DEVICES=7 python train.py \
+#torchrun --standalone --nproc_per_node=4 train.py \
+#    --opt dash \
+#    --family transformer \
+#    --micro_batch_size 32 \
+#    --train_batches 2000 \
+#    --k_input 1e-3 \
+#    --momentum 0.95 \
+#    --beta2 0.95 \
+#    --backend pytorch \
+#    --update_freq 1 \
+#    --model_device_index 0 ./out/test
+
+python ./src/train.py \
     --opt dash \
-    --micro_batch_size 32 \
+    --micro_batch_size 128 \
     --train_batches 2000 \
-    --k_input 1e-3 \
-    --momentum 0.95 \
+    --model_device_index 0 \
+    --k_input 3e-3 \
+    --momentum 0.9 \
     --beta2 0.95 \
-    --model_device_index 0 ./out/test
+    --resume false \
+    --backend pytorch ./out/test
